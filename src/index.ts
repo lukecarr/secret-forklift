6811b8999a02ce741a08815b812893f28e22c96a
Luke Carr <luke@omniointeractive.com>
2020-03-17 08:11:44 +0000

    Added method for loading parameters synchronously

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import defaults from './defaults';
 const awsParamStore = require('aws-param-store');
 
+const loadDefaults = function(options: SecretForkliftOptions) {
+  return Object.assign(defaults, options);
+}
+
+const handleParamters = function(options: SecretForkliftOptions, parameters: any) {
+  parameters.forEach((parameter: any) => {
+    const name = parameter.Name.split('/');
+    const value = parameter.Value;
+    options.loader(options.formatter(name[name.length - 1]), value);
+  });
+}
+
 export const load = function(options: SecretForkliftOptions) {
   return new Promise((resolve, reject) => {
-    options = Object.assign(defaults, options);
+    options = loadDefaults(options);
     awsParamStore.getParametersByPath(options.path).then((parameters: any) => {
-      parameters.forEach((parameter: any) => {
-        const name = parameter.Name.split('/');
-        const value = parameter.Value;
-        options.loader(options.formatter(name[name.length - 1]), value);
-      });
+      handleParamters(options, parameters);
       resolve();
     }).catch((err: any) => reject(err));
   });
+};
+
+export const loadSync = function(options: SecretForkliftOptions) {
+  options = loadDefaults(options);
+  const parameters = awsParamStore.getParametersByPathSync(options.path);
+  handleParamters(options, parameters);
 };
