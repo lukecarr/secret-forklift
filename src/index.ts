66e967162be8a69f657de30e94f8f3dc552c84ab
Luke Carr <luke@omniointeractive.com>
2020-03-29 17:55:32 +0100

    Applied Prettier rules to existing codebase

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { mergeDefaults, SecretForkliftOptions, Parameter } from './options';
+import { mergeDefaults, SecretForkliftOptions, Parameter } from "./options";
 
-const awsParamStore = require('aws-param-store');
+const awsParamStore = require("aws-param-store");
 
-const handleParamters = (options: SecretForkliftOptions, parameters: Parameter[]): void => {
+const handleParamters = (
+  options: SecretForkliftOptions,
+  parameters: Parameter[]
+): void => {
   parameters.forEach((parameter: Parameter) => {
     const parameterFormatted: Parameter = parameter;
-    const nameSplit: string[] = parameterFormatted.Name.split('/');
-    parameterFormatted.Name = options.formatter(nameSplit[nameSplit.length - 1]);
+    const nameSplit: string[] = parameterFormatted.Name.split("/");
+    parameterFormatted.Name = options.formatter(
+      nameSplit[nameSplit.length - 1]
+    );
     options.loader(parameterFormatted);
   });
 };
@@ -20,10 +25,13 @@ const handleParamters = (options: SecretForkliftOptions, parameters: Parameter[]
 export const load = (options?: SecretForkliftOptions): Promise<void> => {
   return new Promise((resolve, reject) => {
     const optionsWithDefaults = mergeDefaults(options);
-    awsParamStore.getParametersByPath(optionsWithDefaults.path).then((parameters: Parameter[]) => {
-      handleParamters(optionsWithDefaults, parameters);
-      resolve();
-    }).catch((err: any) => reject(err));
+    awsParamStore
+      .getParametersByPath(optionsWithDefaults.path)
+      .then((parameters: Parameter[]) => {
+        handleParamters(optionsWithDefaults, parameters);
+        resolve();
+      })
+      .catch((err: any) => reject(err));
   });
 };
 
@@ -34,6 +42,8 @@ export const load = (options?: SecretForkliftOptions): Promise<void> => {
  */
 export const loadSync = (options?: SecretForkliftOptions): void => {
   const optionsWithDefaults = mergeDefaults(options);
-  const parameters = awsParamStore.getParametersByPathSync(optionsWithDefaults.path);
+  const parameters = awsParamStore.getParametersByPathSync(
+    optionsWithDefaults.path
+  );
   handleParamters(optionsWithDefaults, parameters);
 };
--- src/options.ts
+++ src/options.ts
@@ -1,7 +1,7 @@
 type NameFormatter = (name: string) => string;
 type ParameterLoader = (parameter: Parameter) => void;
 
-type ParameterType = 'String' | 'StringList' | 'SecureString';
+type ParameterType = "String" | "StringList" | "SecureString";
 
 export interface SecretForkliftOptions {
   path: string;
@@ -20,20 +20,25 @@ export interface Parameter {
 }
 
 export const defaults: SecretForkliftOptions = {
-  path: process.env.AWS_PARAM_STORE_PATH || '',
+  path: process.env.AWS_PARAM_STORE_PATH || "",
   override: true,
   formatter(name) {
-    return name.replace(/-/g, '_').toUpperCase();
+    return name.replace(/-/g, "_").toUpperCase();
   },
   loader(parameter) {
-    if ((Object.prototype.hasOwnProperty.call(process.env, parameter.Name) && this.override)
-      || !Object.prototype.hasOwnProperty.call(process.env, parameter.Name)) {
+    if (
+      (Object.prototype.hasOwnProperty.call(process.env, parameter.Name) &&
+        this.override) ||
+      !Object.prototype.hasOwnProperty.call(process.env, parameter.Name)
+    ) {
       process.env[parameter.Name] = parameter.Value;
     }
   },
 };
 
-export const mergeDefaults = (options?: SecretForkliftOptions): SecretForkliftOptions => {
+export const mergeDefaults = (
+  options?: SecretForkliftOptions
+): SecretForkliftOptions => {
   const newOptions: any = { ...options };
   Object.entries(defaults).forEach(([key, value]) => {
     if (!Object.prototype.hasOwnProperty.call(newOptions, key)) {
