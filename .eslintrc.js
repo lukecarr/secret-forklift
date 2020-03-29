d106a0fa22a9d2f9937ffff7e866b5785f6bca0f
Luke Carr <luke@omniointeractive.com>
2020-03-29 17:55:23 +0100

    Added Prettier for opinionated formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   "extends": [
     "airbnb-base",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
   ],
   "parser": "@typescript-eslint/parser",
   "plugins": [
-    "@typescript-eslint"
+    "@typescript-eslint",
   ],
   "rules": {
     "@typescript-eslint/no-explicit-any": "off",
